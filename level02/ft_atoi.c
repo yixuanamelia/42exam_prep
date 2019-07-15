38d17dab4b36fc714e267be720b838a6effe23e0
yixuan WANG <yixuanamelia@gmail.com>
2019-07-15 12:57:55 +0200

    fixed atoi

--- level02/ft_atoi.c
+++ level02/ft_atoi.c
@@ -1,36 +1,27 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yiwang <marvin@42.fr>                      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/13 15:26:31 by yiwang            #+#    #+#             */
-/*   Updated: 2019/07/13 15:26:35 by yiwang           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include <stdlib.h>
 #include <stdio.h>
-int		ft_atoi(char *str)
+#include <stdlib.h>
+int		ft_atoi(const char *str)
 {
 	int		i;
 	int		nbr;
 	int		sign;
 
-	i = 0;
 	nbr = 0;
 	sign = 1;
+
 	if (str == '\0')
 		return (0);
-	while (str[i] == '\t' || str[i] == '\v' || str[i] == '\n'
-			|| str[i] == '\r' || str[i] == '\f' || str[i] == ' ')
-		i += 1;
-	if (str[i] == '-' || str[i] == '+')
-		if (str[i++] == '-')
-			sign = -1;
-	while (str[i] >= '0' && str[i] <= '9')
-		nbr = (nbr * 10) + (str[i++] - '0');
+	while (*str && (*str == '\f' || *str == '\n' || *str == '\t' || *str == '\v' || *str == ' ' || *str == '\r'))
+		str++;
+	if (*str == '-')
+		sign = -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str && *str >= '0' && *str <= '9')
+	{
+		nbr = (nbr * 10) + (*str - '0');
+		str++;
+	}
 	return (nbr * sign);
 }
 
@@ -38,5 +29,5 @@ int		main(int ac, char **av)
 {
 	printf("%d\n", ft_atoi(av[1]));
 	printf("%d\n", atoi(av[1]));
-	return (0);
+	return 0;
 }
