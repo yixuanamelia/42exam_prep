02c52ecc4951a63748ee47f2e0eaa0a777e8bc70
Yixuan WANG <yiwang@e1r9p8.42.fr>
2019-07-21 17:45:14 +0200

    added rev_wstr

---
+++ level04/rev_wstr_easy.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+void	rev_wstr(char *str, int first)
+{
+	int		start;
+	int		i;
+
+	i = 0;
+	while (str[i] && str[i] == ' ')
+		i++;
+	if (!str[i])
+		return;
+	start = i;
+	while (str[i] && str[i] != ' ')
+		i++;
+	rev_wstr(str + i, 0);
+	write(1, str + start, i - start);
+	if (!first)
+		write(1, " ", 1);
+}
+
+int		main(int ac, char **av)
+{
+	if (ac == 2)
+		rev_wstr((char *)av[1], 1);
+	write(1, "\n", 1);
+	return 0;
+}
